f634ec8367e0a8face0b2e6603f2d1f1282bad52
daniyl-x <daniyl-x@proton.me>
2024-05-24 11:42:35 +0300

    Sort routes in alphabetical order

--- main.py
+++ main.py
@@ -15,20 +15,20 @@ def index():
     return data
 
 
-@app.get("/ping")
-def ping():
-    ip = request.args.get("ip")
-    result = subprocess.run(["ping", "-c", "5", ip], capture_output=True)
+@app.get("/dig")
+def dig():
+    domain = request.args.get("domain")
+    result = subprocess.run(["dig", domain], capture_output=True)
     data = {"stdout": result.stdout.decode()}
     data = funs.serialize_json(data)
 
     return data
 
 
-@app.get("/dig")
-def dig():
-    domain = request.args.get("domain")
-    result = subprocess.run(["dig", domain], capture_output=True)
+@app.get("/ping")
+def ping():
+    ip = request.args.get("ip")
+    result = subprocess.run(["ping", "-c", "5", ip], capture_output=True)
     data = {"stdout": result.stdout.decode()}
     data = funs.serialize_json(data)
 
