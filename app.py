7969d07637282190df8ae75dc24d58f58bad77ca
daniyl-x <daniyl-x@proton.me>
2024-05-25 18:36:43 +0300

    Make result div easier to look at
    - replace p tag with samp to use mono font
    - add static dir
    - add style for result class to fix result newlines

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from routes.api import api
 from routes.browser import browser
 
 
-app = Flask(__name__)
+app = Flask(
+        __name__,
+        static_url_path="/static",
+)
 
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(browser)
